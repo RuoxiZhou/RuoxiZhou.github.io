a2f81e6a043f75f140a36f5b4d33fca1f0ccd44e
RuoxiZhou <ruz@unc.edu>
2022-12-02 19:44:11 -0500

    init

---
+++ new.js
@@ -0,0 +1,5 @@
+function se() {
+    console.log(document.getElementsByClassName("789")[0]);
+    document.getElementsByClassName("789")[0].className = "789 seblock";;
+  }
+  
---
+++ product.js
@@ -0,0 +1,5 @@
+function search() {
+    console.log(document.getElementsByClassName("456")[0]);
+    document.getElementsByClassName("456")[0].className = "456 seblock";;
+  }
+  
